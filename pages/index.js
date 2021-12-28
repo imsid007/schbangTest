066956d985831536763c1d148f919cd66b5dec92
imsid1996 <siddharth@mybytecode.com>
2021-12-28 10:35:37 +0530

    code clean

--- pages/index.js
+++ pages/index.js
@@ -13,138 +13,136 @@ export default function Home() {
 
 	return (
 		<MainLayout>
-			<div>
-				<LocationTag />
-				<PageHead />
+			{/* Fixed location tag */}
+			<LocationTag />
+			{/* Pager header  */}
+			<PageHead />
 
-				<Row align="center" className={styles.header_row}>
-					<div className={styles.header_left_column}>
-						<Row
-							align="center"
-							justify="center"
-							className={styles.header_left_column_container}
-						>
-							<div style={{ zIndex: "50" }}>
-								<span className={styles.title}>
-									Discover Restaurant & Delicious Food
-								</span>
-								<div className={styles.header_input_container}>
-									<input
-										className={styles.header_input}
-										placeholder="search restaurant,food"
-									/>
-									<button className={styles.header_submit_btn}>Go</button>
-								</div>
-							</div>
-						</Row>
-					</div>
-					<div
-						style={{ height: "100%", position: "relative" }}
-						className={styles.header_right_column}
+			{/* Header section  */}
+			<Row align="center" className={styles.header_row}>
+				<div className={styles.header_left_column}>
+					<Row
+						align="center"
+						justify="center"
+						className={styles.header_left_column_container}
 					>
-						<div className={styles.header_image_wrapper}>
-							<div className={styles.header_image_container}>
-								<Image
-									layout="fill"
-									alt="salad-plate"
-									src="/images/salad-plate.svg"
-								/>
-							</div>
-						</div>
-						<div className={styles.header_background_wrapper}>
-							<div className={styles.header_background_container}>
-								<Image layout="fill" src="/images/long-dots.svg" alt="dots" />
-							</div>
-						</div>
-
-						<div className={styles.red_column}></div>
-					</div>
-				</Row>
-
-				<Row
-					style={{ paddingTop: "50px", minHeight: "100vh" }}
-					justify="center"
-				>
-					<div className={styles.sub_title_wrapper}>
-						<div className={styles.sub_title}>
-							some top restaurant for dining out or in!
-						</div>
-					</div>
-
-					<div className={styles.description}>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-						ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-					</div>
-
-					<div style={{ position: "relative", width: "100vw" }}>
-						<div className={styles.resto_top_background_wrapper}>
-							<div className={styles.resto_top_background_container}>
-								<Image
-									layout="fill"
-									objectFit="contain"
-									alt="dots image"
-									src="/images/dots.svg"
-								/>
-							</div>
-						</div>
-						<div className={styles.resto_bottom_background_wrapper}>
-							<div className={styles.resto_bottom_background_container}>
-								<Image layout="fill" src="/images/dots.svg" alt="dots" />
-							</div>
-						</div>
-						<div className={styles.resto_card_wrapper}>
-							{RESTO.slice(0, restoCount).map((resto) => (
-								<RestoCards
-									key={Math.random}
-									name={resto.name}
-									url={resto.url}
-								/>
-							))}
-							<div
-								className={styles.see_more_container}
-								onClick={() => {
-									setRestoCount(resto.length);
-								}}
-							>
-								<Image
-									layout="fill"
-									objectFit="contain"
-									src="/images/see-more-logo.svg"
-									alt="see-more"
-								/>
-							</div>
-						</div>
-					</div>
-				</Row>
-				<Row align="center" justify="center" className={styles.footer_section}>
-					<div className={styles.form_container}>
-						<div className={styles.color_head}></div>
-						<div className={styles.footer_title}>advanced booking</div>
-						<div>
-							<div className={styles.footer_input_container}>
+						<div style={{ zIndex: "50" }}>
+							<span className={styles.title}>
+								Discover Restaurant & Delicious Food
+							</span>
+							<div className={styles.header_input_container}>
 								<input
-									className={styles.footer_input}
-									placeholder="search restaurant"
+									className={styles.header_input}
+									placeholder="search restaurant,food"
 								/>
-								<button className={styles.footer_submit_btn}>Go</button>
+								<button className={styles.header_submit_btn}>Go</button>
 							</div>
 						</div>
+					</Row>
+				</div>
+				<div
+					style={{ height: "100%", position: "relative" }}
+					className={styles.header_right_column}
+				>
+					<div className={styles.header_image_wrapper}>
+						<div className={styles.header_image_container}>
+							<Image
+								layout="fill"
+								alt="salad-plate"
+								src="/images/salad-plate.svg"
+							/>
+						</div>
 					</div>
-					<div className={styles.left_veggi_wrapper}>
-						<div className={styles.left_veggi_container} style={{}}>
+					<div className={styles.header_background_wrapper}>
+						<div className={styles.header_background_container}>
+							<Image layout="fill" src="/images/long-dots.svg" alt="dots" />
+						</div>
+					</div>
+
+					<div className={styles.red_column}></div>
+				</div>
+			</Row>
+			{/* Header section end  */}
+
+			{/* Partner restorants section  */}
+			<Row style={{ paddingTop: "50px", minHeight: "100vh" }} justify="center">
+				<div className={styles.sub_title_wrapper}>
+					<div className={styles.sub_title}>
+						some top restaurant for dining out or in!
+					</div>
+				</div>
+
+				<div className={styles.description}>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+					eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+					minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+				</div>
+
+				<div style={{ position: "relative", width: "100vw" }}>
+					<div className={styles.resto_top_background_wrapper}>
+						<div className={styles.resto_top_background_container}>
+							<Image
+								layout="fill"
+								objectFit="contain"
+								alt="dots image"
+								src="/images/dots.svg"
+							/>
+						</div>
+					</div>
+					<div className={styles.resto_bottom_background_wrapper}>
+						<div className={styles.resto_bottom_background_container}>
+							<Image layout="fill" src="/images/dots.svg" alt="dots" />
+						</div>
+					</div>
+					<div className={styles.resto_card_wrapper}>
+						{RESTO.slice(0, restoCount).map((resto) => (
+							<RestoCards key={Math.random} name={resto.name} url={resto.url} />
+						))}
+						<div
+							className={styles.see_more_container}
+							onClick={() => {
+								setRestoCount(resto.length);
+							}}
+						>
+							<Image
+								layout="fill"
+								objectFit="contain"
+								src="/images/see-more-logo.svg"
+								alt="see-more"
+							/>
+						</div>
+					</div>
+				</div>
+			</Row>
+
+			{/* Search restorant section  */}
+			<Row align="center" justify="center" className={styles.footer_section}>
+				<div className={styles.form_container}>
+					<div className={styles.color_head}></div>
+					<div className={styles.footer_title}>advanced booking</div>
+					<div>
+						<div className={styles.footer_input_container}>
+							<input
+								className={styles.footer_input}
+								placeholder="search restaurant"
+							/>
+							<button className={styles.footer_submit_btn}>Go</button>
+						</div>
+					</div>
+				</div>
+				<div className={styles.left_veggi_wrapper}>
+					<div className={styles.left_veggi_container} style={{}}>
+						<Image layout="fill" src="/images/test2.png" alt="iceberg" />
+					</div>
+				</div>
+				<div>
+					<div className={styles.right_veggi_wrapper}>
+						<div className={styles.right_veggi_container}>
 							<Image layout="fill" src="/images/test2.png" alt="iceberg" />
 						</div>
 					</div>
-					<div>
-						<div className={styles.right_veggi_wrapper}>
-							<div className={styles.right_veggi_container}>
-								<Image layout="fill" src="/images/test2.png" alt="iceberg" />
-							</div>
-						</div>
-					</div>
-				</Row>
-			</div>
+				</div>
+			</Row>
 		</MainLayout>
 	);
 }
